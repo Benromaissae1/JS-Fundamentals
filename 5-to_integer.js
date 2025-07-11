31b6d830c4ee203c949a233591cc78f9d9759749
benromaissae1 <benromaissae1@gmail.com>
2025-07-11 13:40:45 -0700

    task 5

---
+++ 5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const input = process.argv[2];
+
+if (input === undefined) {
+    console.log('Not a number');
+} else {
+    const myVar = parseInt(input);
+    if (isNaN(myVar)) {
+        console.log(`${input} Not a number`);
+    } else {
+        console.log('My number: ' + myVar);
+    }
+}
+
