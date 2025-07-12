5000e36cbe5ad8336f5638bd023d67294cdd2282
benromaissae1 <benromaissae1@gmail.com>
2025-07-12 10:07:03 -0700

    task 9

---
+++ 9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const input = process.argv
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+
+function add(a, b){
+    if (isNaN(a) || isNaN(b)) {
+        console.log(NaN);
+    }
+    else {
+        console.log(a + b);
+    }
+}
+add(a, b);
